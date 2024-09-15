b57903cc1d7b8fa635286aebe8813079146faeb7
Felipe Quérette <felipe.querette@gmail.com>
2024-09-15 20:54:41 -0300

    first commit

---
+++ src/main/java/br/com/fullstack/suggestionbox/SuggestionboxApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.suggestionbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionboxApplication.class, args);
+	}
+
+}
